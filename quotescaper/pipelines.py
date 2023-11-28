bdb84dcab6a966f9f5b68f1c2ea36685dc586790
Gibril1 <gibrilissakaiddrisu@gmail.com>
2023-11-28 15:47:52 +0000

    pipeline setup

--- quotescaper/pipelines.py
+++ quotescaper/pipelines.py
@@ -10,4 +10,18 @@ from itemadapter import ItemAdapter
 
 class QuotescaperPipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        # turn the tags from lists to comma separated strings
+        tags = adapter.get('tags')
+        string_representation = ', '.join(tags)
+        adapter['tags'] = string_representation
+
+        # strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field in field_names:
+            value = adapter.get(field)
+            adapter[field] = value.strip()
+
         return item
