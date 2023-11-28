781c725a6ed7b8b2718f1caa9c0e2bbfc4eb0535
Gibril1 <gibrilissakaiddrisu@gmail.com>
2023-11-28 11:06:49 +0000

    items has been retrieved

--- quotescaper/spiders/quotespider.py
+++ quotescaper/spiders/quotespider.py
@@ -7,4 +7,10 @@ class QuotespiderSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        all_quotes = response.css(".col-md-8 .quote")
+        for quote in all_quotes:
+            yield{
+                'quote_text': quote.css(".text ::text").get(),
+                'author': quote.css(".author::text").get(),
+                'tags': quote.css(".tags a::text").getall(),
+            }
